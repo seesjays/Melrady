79da68bb5bfb3dd26bf064a2a63479f81589f6d0
ChrisO <39662993+ChrisOh431@users.noreply.github.com>
2022-06-10 15:40:10 -0500

    tracksearch now perfectly functional

--- src/search.js
+++ src/search.js
@@ -18,60 +18,60 @@ function search(sharedObjects) {
 		"/search",
 		auth,
 		body("track_1_name", "track_2_name", "track_3_name", "track_4_name")
+			.isString()
+			.isLength({ min: 0, max: 40 })
 			.trim()
 			.blacklist("<>&'\"/")
 			.escape(),
 		async (req, res) => {
 			const errors = validationResult(req);
 			if (!errors.isEmpty()) {
+				console.log("tracksearch: didn't pass validation");
 				return res.status(400).json({
 					message: "tracksearch: error - invalid inputs",
 					errors: errors.array(),
 				});
 			}
 
-			console.log(
-				`tracksearch: searching ${Object.keys(req.body).length} tracks:`
-			);
+			// valid escaped strings with a max length of 40
+			// 40 was a number I picked kind of at random, but it should cover like
+			// 90% of songs, easy.
 
-			console.dir(req.body);
+			const queries = Object.values(req.body).filter(
+				(query) => query.length > 0
+			);
+			console.log(`tracksearch: searching ${queries.length} tracks:`);
+			console.dir(queries);
 
 			const { access_tok_key } = require("./cookieMapping").cookieMap;
 			const cookies = req.cookies ? req.cookies : null;
 			const access_token = cookies ? cookies[access_tok_key] : null;
 
 			const { createTrackObject } = require("./trackObject");
-			let trackouts = [];
-
 			const output = await Promise.all(
-				Object.values(req.body)
-					.filter((query) => query.length > 0)
-					.map(async (query, index) => {
-						spotify_api.setAccessToken(access_token);
+				queries.map(async (query) => {
+					spotify_api.setAccessToken(access_token);
+					const searchResults = await spotify_api.searchTracks(query, {
+						limit: 3,
+					});
 
-						const searchResults = await spotify_api.searchTracks(query, {
-							limit: 3,
-						});
+					// search results are massive, so we filter down the values
+					// into nice packaged objects using createTrackObject
 
-						if (searchResults) {
-							for (const searchresult of searchResults.body.tracks.items) {
-								const trackobj = createTrackObject(searchresult);
-
-								console.log(trackobj);
-							}
-							return searchResults;
-						} else {
-							for (let i = 0; i < 3; i++) {
-								let trackobj = create_dummy_track_obj();
-								trackobj.search_box_num = trackin[0][6];
-
-								search_results.push(trackobj);
-							}
-						}
-					})
+					if (searchResults) {
+						const trackObjects = Object.values(
+							searchResults.body.tracks.items
+						).map((fullData) => createTrackObject(fullData));
+						return trackObjects;
+					} else {
+						return null;
+					}
+				})
 			);
-
-			console.log(`search: Successfully searched ${output.length} songs.`);
+			
+			// Promise.all returns everything in the same order it was fed, which is nice.
+			console.log(`tracksearch: returning ${output.length} track objects:`);
+			console.dir(output);
 			return res.json(output);
 		}
 	);
